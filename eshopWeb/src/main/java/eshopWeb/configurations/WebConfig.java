65bde2d56cf083397d38ac173885eb133c106b56
olivier <o.gozlan@ajc-ingenierie.fr>
2023-04-04 12:16:43 +0200

    spring mvc

---
+++ eshopWeb/src/main/java/eshopWeb/configurations/WebConfig.java
@@ -0,0 +1,24 @@
+package eshopWeb.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+@Configuration
+//activation des annotations de spring mvc
+@EnableWebMvc
+@ComponentScan("eshopWeb.controllers")
+public class WebConfig {
+
+	@Bean
+	public UrlBasedViewResolver viewResolver() {
+		UrlBasedViewResolver viewResolver = new UrlBasedViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+}
